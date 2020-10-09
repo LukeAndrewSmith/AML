c7ff4323c425d3f15852eff3a6134cc7830d058b
Luke.Smith <luke.smith@bluewin.ch>
2020-10-09 13:18:45 +0000

    Mean impute

--- Task_1/Solution/Components/Imputation.py
+++ Task_1/Solution/Components/Imputation.py
@@ -1,5 +1,10 @@
-from sklearn.impute import SimpleImputer
-import np
+klearn.impute import SimpleImputer
+import numpy as np
+import pandas as pd
+
+def mean(x):
+    fill_NaN = SimpleImputer(missing_values=np.nan, strategy='mean')
+    return pd.DataFrame(fill_NaN.fit_transform(x))
 
 def median(x):
     fill_NaN = SimpleImputer(missing_values=np.nan, strategy='median')
