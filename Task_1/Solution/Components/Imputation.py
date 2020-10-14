d15a3c2ed3f4f4547ac1ecc78b3a8f80de9e9136
Luke.Smith <luke.smith@bluewin.ch>
2020-10-14 08:52:06 +0000

    More Progress

--- Task_1/Solution/Components/Imputation.py
+++ Task_1/Solution/Components/Imputation.py
@@ -7,22 +7,30 @@ import pandas as pd
 import numpy as np
 
 def mean(X, x_supp=None):
+    if x_supp is not None:
+        x_supp.columns = X.columns
     imp = SimpleImputer(missing_values=np.nan, strategy='mean')
     imp.fit(pd.concat([X,x_supp],ignore_index=True))
     return pd.DataFrame(imp.transform(X))
 
 def median(X, x_supp=None):
+    if x_supp is not None:
+        x_supp.columns = X.columns    
     imp = SimpleImputer(missing_values=np.nan, strategy='median')
     imp.fit(pd.concat([X,x_supp],ignore_index=True))
     return pd.DataFrame(imp.transform(X))
 
-def iterative_regression(X,  x_supp=None): # default regressor: BayesianRidge
+def iterative_regression(X,  x_supp=None, n_nearest_features=10): # default regressor: BayesianRidge
+    if x_supp is not None:
+        x_supp.columns = X.columns     
     imp = IterativeImputer(missing_values=np.nan, 
-                            max_iter=10, initial_strategy='median',random_state=0)
+                            max_iter=10, initial_strategy='median',random_state=0, n_nearest_features=n_nearest_features)
     imp.fit(pd.concat([X,x_supp],ignore_index=True))
     return pd.DataFrame(imp.transform(X))
 
-def knn(X, x_supp=None):
+def knn(X, x_supp=None, neighbors=1):
+    if x_supp is not None:
+        x_supp.columns = X.columns     
     imp = KNNImputer(missing_values=np.nan, weights='distance')
     imp.fit(pd.concat([X,x_supp],ignore_index=True))
     return pd.DataFrame(imp.transform(X))
@@ -32,5 +40,32 @@ def missing_values_mask(X):
     return indicator.fit_transform(X)
 
 
+def mean2(X, x_supp):
+    if x_supp is not None:
+        x_supp.columns = X.columns
+    imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+    imp.fit(pd.concat([X,x_supp],ignore_index=True))
+    return pd.DataFrame(imp.transform(X)), pd.DataFrame(imp.transform(x_supp))
 
+def median2(X, x_supp):
+    if x_supp is not None:
+        x_supp.columns = X.columns    
+    imp = SimpleImputer(missing_values=np.nan, strategy='median')
+    imp.fit(pd.concat([X,x_supp],ignore_index=True))
+    return pd.DataFrame(imp.transform(X)), pd.DataFrame(imp.transform(x_supp))
+
+def iterative_regression2(X,  x_supp, n_nearest_features=10): # default regressor: BayesianRidge
+    if x_supp is not None:
+        x_supp.columns = X.columns     
+    imp = IterativeImputer(missing_values=np.nan, 
+                            max_iter=10, initial_strategy='median',random_state=0, n_nearest_features=n_nearest_features)
+    imp.fit(pd.concat([X,x_supp],ignore_index=True))
+    return pd.DataFrame(imp.transform(X)), pd.DataFrame(imp.transform(x_supp))
+
+def knn2(X, x_supp, neighbors=1):
+    if x_supp is not None:
+        x_supp.columns = X.columns     
+    imp = KNNImputer(missing_values=np.nan, weights='distance',n_neighbors=neighbors)
+    imp.fit(pd.concat([X,x_supp],ignore_index=True))
+    return pd.DataFrame(imp.transform(X)), pd.DataFrame(imp.transform(x_supp))
 
--- Task_1/Solution/Components/Outlier_Detection_ND.py
+++ Task_1/Solution/Components/Outlier_Detection_ND.py
@@ -12,7 +12,46 @@ def _get_cov(X):
     cov = EmpiricalCovariance().fit(X)
     return cov
 
-def mahalanobis_distance(X_train, y_train, alpha=3, verbose=False):
+def magic_indices(X_train,y_train, n_outliers=10, mask=None, verbose=False):
+    # gained from loocv 
+    # 180 seems to be best for lasso 
+    magic_indices = [323, 333, 215, 1094, 1179, 774, 527, 826, 981, 673, 313, 765, 
+                    1022, 487, 627, 657, 1199, 431, 42, 789, 240, 923, 989, 890, 988, 
+                    317, 1182, 16, 412, 809, 120, 62, 357, 1078, 1000, 136, 818, 571, 
+                    344, 34, 942, 1189, 845, 1042, 204, 537, 960, 1201, 390, 908, 990, 
+                    611, 811, 880, 875, 1021, 1205, 968, 884, 270, 1115, 639, 200, 1067, 
+                    1117, 1070, 1130, 947, 559, 1098, 920, 927, 854, 1195, 1100, 900, 1157, 
+                    899, 1036, 869, 384, 251, 911, 1135, 228, 1009, 1209, 1128, 1197, 733, 
+                    1063, 859, 905, 459, 146, 110, 1113, 1096, 1093, 1162, 889, 821, 7, 993, 
+                    1047, 962, 914, 812, 372, 1178, 1148, 903, 1172, 820, 1198, 677, 1003, 
+                    1069, 825, 1210, 1145, 1028, 340, 1065, 46, 936, 800, 860, 1134, 898, 
+                    616, 628, 1046, 810, 1207, 963, 1077, 1196, 1177, 984, 1165, 217, 885, 9,
+                    50, 1048, 1141, 1138, 964, 1149, 1171, 819, 965, 133, 206, 1083, 866, 852,
+                    895, 1183, 1176, 907, 202, 1005, 153, 1109, 670, 1010, 874, 856, 1190, 1091,
+                    967, 233, 1184, 1033, 814, 886, 179, 385, 1150, 904, 992, 1044, 980, 1079, 
+                    1081, 1020, 983, 1159, 994, 35, 896, 1174, 282, 1106, 901, 867, 827, 954, 
+                    939, 834, 871, 970, 858, 1049, 822, 1122, 1200, 1180, 1019, 1034, 958, 969, 
+                    868, 892, 974, 1131, 1187, 1076, 1105, 953, 1164, 955, 979, 833, 1025, 816, 
+                    973, 1158, 951, 1055, 862, 181, 1037, 1040, 1124, 361, 1206, 1006, 1032, 815, 
+                    1154, 1168, 878, 319, 131, 891, 837, 1085, 597]
+    
+    # take first n_outliers
+    selected = magic_indices[0:n_outliers]
+    outliers = np.asarray(selected)
+    if verbose:
+        print('# Outliers: ', outliers)
+        
+    X = X_train.drop(X_train.index[outliers])
+    y = y_train.drop(y_train.index[outliers])
+
+    if mask is None:
+        return X,y
+    else:
+        mask = pd.DataFrame(mask).drop(y_train.index[outliers]).to_numpy()
+        return X,y,mask
+
+
+def mahalanobis_distance(X_train, y_train, mask='', alpha=3, verbose=False):
     #join x train and y train
     train_all = X_train.copy()
     train_all['age'] = y_train
@@ -31,14 +70,19 @@ def mahalanobis_distance(X_train, y_train, alpha=3, verbose=False):
     
     X = X_train.drop(X_train.index[outliers])
     y = y_train.drop(y_train.index[outliers])
+    if mask == '':
+        return X,y
+    else:
+        mask = pd.DataFrame(mask).drop(y_train.index[outliers]).to_numpy()
+        return X,y,mask
 
-    return X,y
 
-
-def dbscan(X_train, y_train, verbose=False):
+def dbscan(X_train, y_train, mask='', verbose=False):
     from sklearn.cluster import DBSCAN
     train_all = X_train.copy()
-    clustering = DBSCAN(eps=3, min_samples=2).fit(X)
+    train_all['age'] = y_train
+
+    clustering = DBSCAN(eps=42, min_samples=2).fit(train_all)
     labels = clustering.labels_
     if verbose:
         print(labels)
@@ -50,4 +94,32 @@ def dbscan(X_train, y_train, verbose=False):
     X = X_train.drop(X_train.index[outliers])
     y = y_train.drop(y_train.index[outliers])
 
-    return X,y
+    if mask == '':
+        return X,y
+    else:
+        mask = pd.DataFrame(mask).drop(y_train.index[outliers]).to_numpy()
+        return X,y,mask
+
+
+def isolation_forest(X_train, y_train, mask='', verbose=False):
+    from sklearn.ensemble import IsolationForest
+    train_all = X_train.copy()
+    train_all['age'] = y_train
+
+    clf = IsolationForest(n_estimators=10, warm_start=True)
+    clf.fit(train_all)  # fit 10 trees  
+    clf.set_params(n_estimators=20)  # add 10 more trees  
+    clf.fit(train_all)
+    labels = clf.predict(train_all)
+    outliers = np.where(np.asarray(labels)==-1)
+    if verbose:
+        print('# Outliers: ', outliers)
+
+    X = X_train.drop(X_train.index[outliers])
+    y = y_train.drop(y_train.index[outliers])
+    if mask == '':
+        return X,y
+    else:
+        mask = pd.DataFrame(mask).drop(y_train.index[outliers]).to_numpy()
+        return X,y,mask
+   
