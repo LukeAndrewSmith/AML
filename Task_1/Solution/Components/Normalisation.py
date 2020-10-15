d5b2d2a4569337f69c25aedff218be12f0e761ff
Luke.Smith <luke.smith@bluewin.ch>
2020-10-15 07:10:22 +0000

    Stuff

--- Task_1/Solution/Components/Normalisation.py
+++ Task_1/Solution/Components/Normalisation.py
@@ -1,4 +1,4 @@
-from sklearn.preprocessing import QuantileTransformer
+from sklearn.preprocessing import QuantileTransformer, PowerTransformer
 import numpy as np
 import pandas as pd
 
@@ -8,3 +8,7 @@ def select():
 def gaussian(X):
     qt = QuantileTransformer(n_quantiles=100, output_distribution='normal')
     return pd.DataFrame(qt.fit_transform(X),columns=X.columns)
+
+def yeo_johnson(X, standardize=True):
+    yeo = PowerTransformer(standardize=standardize)
+    return pd.DataFrame(yeo.fit_transform(X), columns=X.columns)
