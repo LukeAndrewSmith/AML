bc2db44f132258dd36aab78b5ccec663dfacc286
Luke.Smith <luke.smith@bluewin.ch>
2020-10-15 12:49:34 +0000

    Sklearn pipeline optimisation

--- Task_1/Solution/Components/Outlier_Detection_1D.py
+++ Task_1/Solution/Components/Outlier_Detection_1D.py
@@ -13,23 +13,23 @@ def z_score(X,x_extra=None,verbose=False):
         print('# Outliers: ', (z_scores>threashold).sum().sum())
     return X.mask(z_scores>threashold)
 
-class Z_Score_Outlier():
-    def __init__(self, percentiles=3):
-        self.percentiles = percentiles
+# class Z_Score_Outlier():
+#     def __init__(self, percentiles=3):
+#         self.percentiles = percentiles
 
-    def fit(self, X, Y):
-       return self
+#     def fit(self, X, Y):
+#        return self
 
-    def transform(self, X, Y=None):
-        try:
-            if self.features < X.shape[1]:
-                if Y is not None:
-                    self.selector.fit(X, Y)
-                return selector.transform(X)
-        except Exception as err:
-            print('MyFeatureSelector.transform(): {}'.format(err))
-       return X
+#     def transform(self, X, Y=None):
+#         try:
+#             if self.features < X.shape[1]:
+#                 if Y is not None:
+#                     self.selector.fit(X, Y)
+#                 return selector.transform(X)
+#         except Exception as err:
+#             print('MyFeatureSelector.transform(): {}'.format(err))
+#        return X
 
-    def fit_transform(self, X, Y=None):
-        self.fit(X, Y)
-        return self.transform(X, Y)
+#     def fit_transform(self, X, Y=None):
+#         self.fit(X, Y)
+#         return self.transform(X, Y)
