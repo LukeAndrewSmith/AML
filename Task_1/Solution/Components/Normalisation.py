f5fc23ab2ea4828820601f8bd51e26d3dd2d65a0
Luke.Smith <luke.smith@bluewin.ch>
2020-10-17 09:59:02 +0000

    Outlier detection post feature selection

--- Task_1/Solution/Components/Normalisation.py
+++ Task_1/Solution/Components/Normalisation.py
@@ -5,7 +5,7 @@ import pandas as pd
 def select():
     print("Hi")
 
-def gaussian(X,n_quants=100):
+def gaussian(X,n_quants=50):
     qt = QuantileTransformer(n_quantiles=n_quants, output_distribution='normal')
     return pd.DataFrame(qt.fit_transform(X),columns=X.columns)
 
