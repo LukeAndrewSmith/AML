f20095f100ee902e7462053c427fc5851e2fd8cf
LukeAndrewSmith <luke.smith@bluewin.ch>
2020-11-23 16:06:14 +0000

    Progress

--- Task_3/Solution/Components/Feature_Engineering.py
+++ Task_3/Solution/Components/Feature_Engineering.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import neurokit2 as nk
 from multiprocessing import Pool
+from pprint import pprint
 
 def extract_features(X=None):
     X_2 = []
@@ -13,7 +14,8 @@ def extract_features(X=None):
             signals,_ = nk.ecg_process(row.dropna(), sampling_rate=300)
             peak_types = ["ECG_P_Peaks","ECG_Q_Peaks","ECG_R_Peaks","ECG_S_Peaks","ECG_T_Peaks"]
             peaks = [list(signals.index[signals[peak_type]==1]) for peak_type in peak_types]
-            X_2.append(extract_features_peaks(index,peaks))
+            ecg_clean = signals[['ECG_Clean']]
+            X_2.append(extract_features_peaks(index,peaks,ecg_clean))
         except Exception as inst:
             print("Index failed: ",index, "with error: \n", inst)
             index_failed.append(index)
@@ -38,54 +40,85 @@ def process_row(index,row):
         signals,_ = nk.ecg_process(row, sampling_rate=300)
         peak_types = ["ECG_P_Peaks","ECG_Q_Peaks","ECG_R_Peaks","ECG_S_Peaks","ECG_T_Peaks"]
         peaks = [list(signals.index[signals[peak_type]==1]) for peak_type in peak_types] 
+        ecg_clean = signals[['ECG_Clean']]
     except Exception as inst:
-        # NeuroKit2 Problems: sometimes it fails miserably during nk.ecg_process() for wierd reasons that by my
-        # investigation shouldn't be happenning.
+        # NeuroKit2 Problems: sometimes it fails miserably during nk.ecg_process() for weird reasons that by my
+        # investigation shouldn't be occuring.
         # TODO: FIND A BETTER LIBRARY (for now just replace with zeros)
         print(index, " failed with error: ", inst)
         peaks = None
-    return extract_features_peaks(index,peaks)
+        ecg_clean = None
+    return extract_features_peaks(index,peaks,ecg_clean)
     
 
-def extract_features_peaks(index,peaks):
+def extract_features_peaks(index,peaks,ecg_clean):
 
-    if peaks is None:
+    if peaks is None or ecg_clean is None:
         return None
     
     features = []
     
+    ################
+    # Random Metrics
+    ################
     if np.mean(np.diff(peaks[2])) != 0:
         bpm = 18000 / np.mean(np.diff(peaks[2]))
     else:
         bpm = 0
     features.append(bpm)
     
-    ######################
-    # Single-peak type measures
-        # For the single peak type measure we wan't all the data that ecg_process found
-            
-    # Variablity measures: single peak type
+    ##########################
+    # Single-peak type metrics
+    ##########################
+    peak_heights = [[x[0] for x in ecg_clean.iloc[p].to_numpy()] for p in peaks]
+
+    # Location
     for i in range(len(peaks)):
         peak = peaks[i]
         diff = np.diff(peak)
-        features.append(np.mean(peak))
-        features.append(np.var(peak))
         features.append(np.mean(diff))
         features.append(np.var(diff))
         
-    #####################
-    # Multi-peak type measures
+    # Scale
+    for i in range(len(peak_heights)):
+        features.append(np.mean(np.abs(peak_heights[i]))) # Mean absolute
+        features.append(np.array([1 for x in np.sign(peak_heights[i]) if x == -1]).sum()) # #postive values
+        features.append(np.array([1 for x in np.sign(peak_heights[i]) if x == 1]).sum()) # #negative values
+        features.append(np.var(peak_heights[i]))
+        
+    # TODO:
+    # 1. Mean absolute value
+    # 2. Number of positive and negative values
+    # 3. Mean doesn't make sense
+    # 4. Base the scale metrics below on similar thinking (mean absolute difference etc.)
+        
+    ##########################
+    # Multi-peak type metrics
+    ##########################
         # For the multi-peak type measure we require the combination of each peak identified at index i
-        # to represent a valid heartbeat <p,q,r,s,t>
+        # to represent a valid heartbeat <p,q,r,s,t> so we clean the data
     peaks = peaks_cleaning(peaks)
-    
-    # Variablity measures: multi peak types
+    peak_heights = [[x[0] for x in ecg_clean.iloc[p].to_numpy()] for p in peaks]
+
+    # Location
     for i in range(len(peaks)):
         for j in range(len(peaks)):
-            diff = np.array(peaks[i])-np.array(peaks[j])
-            features.append(np.mean(diff))
-            features.append(np.var(diff))
+            if i != j:
+                diff = np.array(peaks[i])-np.array(peaks[j])
+                features.append(np.mean(diff))
+                features.append(np.var(diff))
             
+    # Scale
+    for i in range(len(peak_heights)):
+        for j in range(len(peak_heights)):
+            if i != j:
+                diff = np.array(peak_heights[i])-np.array(peak_heights[j])
+                features.append(np.mean(diff))
+                features.append(np.var(diff))
+    
+    ##########
+    # Problems
+    ##########
     
     # NeuroKit2 Problems: sometimes it only identifies a single peak for one of the p,q,r,s,t peaks, in which 
     # case a diff will result in [] and so NaNs when mean/var is calculated
@@ -114,8 +147,7 @@ def peaks_cleaning(peaks):
             # if p>q,r,t then we can never for a valid pair with q,r,t so drop q,r,t
                 # repeat until p<q,r,t
                 # same with p<q>r,t, p<q<r>t,
-            # cont until p<q<r<t, then check that p<q<r<t is feasible
-            
+            # cont. until p<q<r<t, then check that p<q<r<t is feasible
     i = -1
     while i < len(peaks[0]):
         i += 1
