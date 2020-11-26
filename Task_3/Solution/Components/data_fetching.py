235b5b9e808cc3716ca203ae9221e034f19422cd
Lea Tamberg <lea.tamberg@yahoo.de>
2020-11-26 09:37:31 +0100

    take only y-values in data fetching (speed up)

--- Task_3/Solution/Components/data_fetching.py
+++ Task_3/Solution/Components/data_fetching.py
@@ -2,9 +2,16 @@ import pandas as pd
 import sys,os
 import numpy as np
 
-def get_train_data():
+def get_train_data(y_only = False):
 
     package_directory = os.path.dirname(os.path.abspath(__file__))
+
+    if y_only:
+        y_train = pd.read_csv(package_directory+'/../../Data/y_train.csv')
+        y_train = y_train.drop('id', 1)
+        y_train = pd.DataFrame(np.ascontiguousarray(y_train), columns=['y'])
+        return y_train
+
 
     x_train = pd.read_csv(package_directory+'/../../Data/X_train.csv')
     x_train = x_train.drop('id', 1)
