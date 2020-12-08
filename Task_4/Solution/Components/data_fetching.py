4fe0aa6183628033b3e3d4abff73b102cd02212c
Lea Tamberg <lea.tamberg@yahoo.de>
2020-12-08 09:21:41 +0100

    remove stuff specific to task 3 from data fetching

--- Task_4/Solution/Components/data_fetching.py
+++ Task_4/Solution/Components/data_fetching.py
@@ -24,19 +24,7 @@ def get_train_data(y_only = False):
                            
     return x_train, y_train
 
-def get_engineered_train_data():
 
-    package_directory = os.path.dirname(os.path.abspath(__file__))
-
-    x_train = pd.read_csv(package_directory+'/../../Data/peak_feat_train.csv')
-
-    y_train = pd.read_csv(package_directory+'/../../Data/y_train.csv')
-    y_train = y_train.drop('id', 1)
-    
-    x_train = pd.DataFrame(np.ascontiguousarray(x_train))
-    y_train = pd.DataFrame(np.ascontiguousarray(y_train), columns=['y'])
-                           
-    return x_train, y_train
 
 def get_test_data():
     package_directory = os.path.dirname(os.path.abspath(__file__))
@@ -48,13 +36,6 @@ def get_test_data():
                            
     return x_test
 
-def get_engineered_test_data():
-    package_directory = os.path.dirname(os.path.abspath(__file__))
 
-    x_test = pd.read_csv(package_directory+'/../../Data/peak_feat_test.csv')
-    
-    x_test = pd.DataFrame(np.ascontiguousarray(x_test))
-                           
-    return x_test
 
 
